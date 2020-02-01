5d1b20164a5aced561a5bd8a6935074f4c2e6683
Sam Sanoop <sams@snyk.io>
2020-02-01 16:45:51 +0000

    fix: check path argument before processing
    hotfix to re mediate command injection

--- index.js
+++ index.js
@@ -9,15 +9,18 @@ module.exports = function(path, opts, cb) {
     opts = {};
   }
 
-  var cmd = module.exports.cmd(path, opts);
-  opts.timeout = opts.timeout || 5000;
-
-  exec(cmd, opts, function(e, stdout, stderr) {
-    if (e) { return cb(e); }
+  if(/;|&|`|\$|\(|\)|\|\||\||!|>|<|\?|\${/g.test(JSON.stringify(path))) {
+    console.log('Input Validation failed, Suspicious Characters found');
+  } else {
+    var cmd = module.exports.cmd(path, opts);
+    opts.timeout = opts.timeout || 5000;
+    exec(cmd, opts, function(e, stdout, stderr) {
+      if (e) { return cb(e); }
     if (stderr) { return cb(new Error(stderr)); }
 
-    return cb(null, module.exports.parse(path, stdout, opts));
+      return cb(null, module.exports.parse(path, stdout, opts));
   });
+}
 };
 
 module.exports.cmd = function(path, opts) {
